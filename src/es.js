c9eb717ef5373a892f2c098cd229587a16320aa6
tamuto <mutomob@gmail.com>
2022-11-25 21:14:33 +0000

    import resolver機能追加

--- src/es.js
+++ src/es.js
@@ -6,6 +6,9 @@ const onResolverPlugin = {
   name: 'resolver',
   setup(build) {
     const path = require('path')
+    const fs = require('fs')
+    const https = require('https')
+    const http = require('http')
 
     build.onResolve({ filter: /^~\// }, args => {
       const newPath = path.join(process.cwd(), 'frontend', args.path.substring(1))
@@ -13,6 +16,36 @@ const onResolverPlugin = {
         return { path: newPath + '.js' }
       }
       return { path: newPath }
+    })
+    build.onResolve({ filter: /^https?:\/\/.*\.js$/}, (args) => {
+      const fname = path.join(process.cwd(), '.cache', ...args.path.split('/').slice(2))
+      if (fs.existsSync(fname)) {
+        console.log(`Using Cache: ${args.path}`)
+        return { path: fname }
+      }
+      const cacheDir = path.join(process.cwd(), '.cache', ...args.path.split('/').slice(2, -1))
+      fs.mkdirSync(cacheDir, { recursive: true })
+
+      const stream = fs.createWriteStream(fname)
+      return new Promise((resolve, reject) => {
+        function fetch(url) {
+          console.log(`Downloading: ${url}`)
+          const lib = url.startsWith('https') ? https : http
+          const req = lib.get(args.path, res => {
+            if ([301, 302, 307].includes(res.statusCode)) {
+              fetch(new URL(res.header.location, url).toString())
+              req.abort()
+            } else if (res.statusCode == 200) {
+              res.on('data', chunk => stream.write(chunk))
+              res.on('end', () => {
+                stream.end()
+                resolve({ path: fname })
+              })
+            }
+          }).on('error', reject)
+        }
+        fetch(args.path)
+      })
     })
   }
 }
