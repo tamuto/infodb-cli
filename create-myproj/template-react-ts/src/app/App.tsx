ba9864067c2c8bbc09a930e6fbc679a1e1ebb6db
tamuto <mutomob@gmail.com>
2024-10-28 09:21:43 +0000

    最終調整

--- create-myproj/template-react-ts/src/app/App.tsx
+++ create-myproj/template-react-ts/src/app/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
 	return (
 		<div className="p-5">
-			<h1 className="text-3xl font-bold underline text-red-400 mb-3">
+			<h1 className="text-3xl font-bold underline text-red-400">
 				Hello world!
 			</h1>
 		</div>
