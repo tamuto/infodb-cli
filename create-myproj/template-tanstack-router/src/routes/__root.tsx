6ff1df4c27cb8d5014bf78fd5a464beaca9f6125
tamuto <mutomob@gmail.com>
2024-12-24 20:28:42 +0000

    不具合修正

--- create-myproj/template-tanstack-router/src/routes/__root.tsx
+++ create-myproj/template-tanstack-router/src/routes/__root.tsx
@@ -7,7 +7,7 @@ export const Route = createRootRoute({
 			<div className="p-2 flex gap-2">
 				<Link to="/" className="[&.active]:font-bold">
 					Home
-				</Link>{" "}
+				</Link>
 				<Link to="/about" className="[&.active]:font-bold">
 					About
 				</Link>
